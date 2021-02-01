2fca7ea5f3f9d9e77ae88aae3a7104fa28f9e65d
imran <imufun2@gmail.com>
2021-02-01 13:13:33 +0600

    TabView UI

---
+++ MovieDB-SwiftUI-Combine/MovieDB-SwiftUI-Combine/View/DiscoverView.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverView.swift
+//  MovieDB-SwiftUI-Combine
+//
+//  Created by Imran on 1/2/21.
+//
+
+import SwiftUI
+
+struct DiscoverView: View {
+    var body: some View {
+        Text("Discover View")
+    }
+}
+
+struct DiscoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscoverView()
+    }
+}
---
+++ MovieDB-SwiftUI-Combine/MovieDB-SwiftUI-Combine/View/MovieView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieView.swift
+//  MovieDB-SwiftUI-Combine
+//
+//  Created by Imran on 1/2/21.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        Text("Moview view")
+    }
+}
+
+struct MovieView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieView()
+    }
+}
