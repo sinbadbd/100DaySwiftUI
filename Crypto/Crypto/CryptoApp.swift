80f5557b55163832c7395ff5149a94bf124a8b5e
Imran <imufun2@gmail.com>
2022-03-22 23:29:29 +0600

    crypto project add

---
+++ Crypto/Crypto/CryptoApp.swift
@@ -0,0 +1,20 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Imran on 22/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
