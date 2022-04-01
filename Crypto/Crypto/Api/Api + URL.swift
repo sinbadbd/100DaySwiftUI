e1f09bb15b20f0af3671cbe22225133fb329c1e6
Imran <imufun2@gmail.com>
2022-04-01 17:13:27 +0600

    [Crypto] Network Generics with api response

--- Crypto/Crypto/Api/Api + URL.swift
+++ Crypto/Crypto/Api/Api + URL.swift
@@ -9,12 +9,10 @@ import Foundation
 
 
 struct Api {
+ 
+    static let BASE_URL = "https://api.coingecko.com/api/v3/"
     
-    static public var shared = Api()
-    
-    private let BASE_URL = "https://api.coingecko.com/api/v3/"
-    
-    public let coins = Api.shared.BASE_URL + "coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
+    static let coins = BASE_URL + "coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
 }
 
 
--- Crypto/Crypto/Services/CoinDataService.swift
+++ Crypto/Crypto/Services/CoinDataService.swift
@@ -19,33 +19,16 @@ class CoinDataService {
     
     private func getCoins(){
         
-        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+        guard let url = URL(string: Api.coins) else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-        
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode <= 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
+        coinSubscription =  NetworkingManager.download(url: url)
             .receive(on: DispatchQueue.main)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { (completion) in
-                switch completion{
-                case .finished:
-                    print(" parse json")
-                    break
-                case .failure(let error):
-                    print("Could not parse ")
-                    print(error.localizedDescription)
-                
-                }
-            } receiveValue: { [weak self] (result) in
-                self?.allCoins = result
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] coinResult in
+                self?.allCoins = coinResult
                 self?.coinSubscription?.cancel()
-            }
+            })
     }
 }
+
+//"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
